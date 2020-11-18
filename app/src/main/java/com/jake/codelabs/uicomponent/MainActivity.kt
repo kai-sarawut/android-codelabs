0b31d71597863c143ea856bad1de0a57081eedf8
Kai mackbook <kaiwdj@gmail.com>
2020-11-18 19:57:23 +0700

    Start project

---
+++ app/src/main/java/com/jake/codelabs/uicomponent/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.jake.codelabs.uicomponent
+
+import android.content.res.Configuration
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+    }
+}
